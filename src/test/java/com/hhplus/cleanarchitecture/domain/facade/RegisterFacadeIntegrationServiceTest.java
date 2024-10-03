423af4ad1d5e29ba7f0f864f6408eaa3dd515cfb
whee <hwiveloper17@naver.com>
2024-10-04 08:45:12 +0900

    [code review] jpa 락을 적용한 메서드 동시 조회시, 조회자체를 못하는 이슈 발생

--- src/test/java/com/hhplus/cleanarchitecture/domain/facade/RegisterFacadeIntegrationServiceTest.java
+++ src/test/java/com/hhplus/cleanarchitecture/domain/facade/RegisterFacadeIntegrationServiceTest.java
@@ -184,4 +184,98 @@ class RegisterFacadeIntegrationServiceTest {
         assertEquals("잔여 좌석이 부족합니다.", exception.getMessage());
 
     }
+
+    @Test
+    @DisplayName("스레드를 돌리면, jpa lock을 건 repository 메서드가 정상 동작하지 않음")
+    @Transactional
+    void codeReview() throws InterruptedException {
+
+        // given
+        long teacherId = 1L;
+        String teacherName = "mangKyu";
+        long lectureId = 1L;
+        long lectureItemId = 1L;
+        String lectureName = "클린아키텍쳐";
+        int capacity = 30;
+        LocalDate openDate = LocalDate.now().plusDays(30);
+        boolean isClose = false;
+        int threadCount = 40;
+
+        // 멤버 생성 로직 추가
+        for (int i = 1; i <= threadCount; i++) {
+            String memberName = "member" + i;
+            MemberCreateDto memberCreateDto = MemberCreateDto.builder()
+                    .name(memberName)
+                    .memberType(MemberType.STUDENT)
+                    .build();
+            memberService.create(memberCreateDto);
+        }
+
+        List<MemberDto> memberAll = memberService.getByAll();
+        assertThat(memberAll.size()).isEqualTo(threadCount);
+
+        // 초기 잔여 좌석을 30으로 설정
+        LectureInfoCreateDto lectureInfoCreateDto = LectureInfoCreateDto.builder()
+                .lectureName(lectureName)
+                .memberId(teacherId)
+                .memberName(teacherName)
+                .lectureId(lectureId)
+                .capacity(capacity)
+                .openDate(openDate)
+                .isClose(isClose)
+                .build();
+
+        LectureInfoDto lectureInfo = registerFacadeService.createLectureInfo(lectureInfoCreateDto);
+        assertThat(lectureInfo.getLectureName()).isEqualTo(lectureName);
+        assertThat(lectureInfo.getCapacity()).isEqualTo(capacity);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        // when
+        for (int i = 1; i <= threadCount; i++) {
+
+            long memberId = i;
+
+            executorService.submit(() -> {
+
+                System.out.println("memberId " + memberId + " - 수강신청 시작");
+
+                LectureApplyDto applyDto = LectureApplyDto.builder()
+                        .memberId(memberId)
+                        .lectureId(lectureId)
+                        .lectureItemId(lectureItemId)
+                        .applyStatus(ApplyStatus.APPLY)
+                        .build();
+                try {
+
+                    /**
+                     * code review 쓰기 락을 적용하니까 lectureInventory 객체를 조회할 수 없어 null이 반환되는 이슈가 발생
+                     */
+                    registerFacadeService.applyLecture(applyDto);
+
+
+                    List<LectureApplyHistoryDto> history = registerFacadeService.getApplyHistoryByMemberId(memberId);
+
+                    assertThat(history.size()).isEqualTo(1);
+                    assertThat(history.get(0).getMemberId()).isEqualTo(memberId);
+                    assertThat(history.get(0).getLectureId()).isEqualTo(lectureId);
+                    assertThat(history.get(0).getLectureItemId()).isEqualTo(lectureItemId);
+
+                } catch (Exception ignored) {
+                    System.out.println("수강 신청 시 예외 발생 = " + ignored.getMessage());
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await(); // 모든 스레드가 작업을 마칠 때까지 대기
+        executorService.shutdown();
+        executorService.awaitTermination(5, TimeUnit.SECONDS);
+
+        // then
+        LectureInventoryDto updatedInventory = lectureInventoryService.getOrThrow(lectureId, lectureItemId);
+        assertThat(updatedInventory.getRemainingSeats()).isEqualTo(0); // 잔여 좌석이 0이어야 함
+    }
 }
